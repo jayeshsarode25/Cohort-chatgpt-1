e95d0ec2b05da98aed3709cc4e7418b6f5c76149
jayeshsarode25 <jayeshsarode2003@gmail.com>
2025-08-18 12:11:03 +0530

    socketIo connection & auth&chat

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieparser = require('cookie-parser');
 const authRouter = require('./routes/auth.route');
+const chatRouter = require('./routes/chat.routes');
 
 
 
@@ -11,5 +12,6 @@ app.use(cookieparser());
 
 
 app.use('/api/auth', authRouter);
+app.use('/api/chat', chatRouter);
 
 module.exports = app;
