1261008abc1748ebab5458906795d34901b78898
jayeshsarode25 <jayeshsarode2003@gmail.com>
2025-09-15 15:01:41 +0530

    backend complete

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -4,7 +4,5 @@ const authController = require('../controller/auth.controller');
 
 const router = express.Router();
 
-router.post('/register', authController.userRegister);
-router.post('/login', authController.userLogin);
 
 module.exports = router;
--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,7 +1,8 @@
-const { GoogleGenAI } = require("@google/genai");
+const { GoogleGenAI } = require("@google/genai")
 
-// The client gets the API key from the environment variable `GEMINI_API_KEY`.
-const ai = new GoogleGenAI({});
+
+const ai = new GoogleGenAI({})
+
 
 async function genreateResponce(content) {
   const response = await ai.models.generateContent({
